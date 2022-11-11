069287306ff5b2053b29c6ae1c2c4956c8e489be
mxsameh <mxsameh@gmail.com>
2022-11-11 02:52:52 +0200

    added images to projects page

--- src/lib/components/home page/Sidebar.svelte
+++ src/lib/components/home page/Sidebar.svelte
@@ -1,126 +1,158 @@
 <script>
-  const shoots = [
-    {
-      title : 'viral photos',
-      link : ''
-    },
-    {
-      title : 'exhibitions',
-      link : ''
-    },
-    {
-      title : 'album covers',
-      link : ''
-
-    },
-    {
-      title : 'commissioned projects',
-      link : ''
-
-    },
-    {
-      title : 'personal projects',
-      link : ''
-
-    },
-    {
-      title : 'live music',
-      link : ''
-
-    },
-    {
-      title : 'portraits',
-      link : ''
-
-    }
-  ]
-
+	const projects = [
+		{
+			title: 'viral photos',
+			link: ''
+		},
+		{
+			title: 'exhibitions',
+			link: ''
+		},
+		{
+			title: 'album covers',
+			link: ''
+		},
+		{
+			title: 'commissioned projects',
+			link: ''
+		},
+		{
+			title: 'personal projects',
+			link: ''
+		},
+		{
+			title: 'live music',
+			link: ''
+		},
+		{
+			title: 'portraits',
+			link: ''
+		}
+	];
 </script>
 
 <!-- flex container -->
-<div class="sidebar"> 
-  <!-- header with logo and subtitle -->
-  <header class="header">
-    <a href="/" class="logo-link">
-      <h1 class="logo">ahmed<br>tarek</h1>
-    </a>
-    <p class="subtitle">photographer</p>
-  </header>
+<div class="sidebar">
+	<!-- header with logo and subtitle -->
+	<header class="header">
+		<a href="/" class="logo-link">
+			<h1 class="logo">ahmed<br />tarek</h1>
+		</a>
+		<p class="subtitle">photographer</p>
+	</header>
 
-  <!-- nav with shoots and projects -->
-  <nav class="shoots-list">
-    {#each shoots as shoot}
-    <a href={shoot.link} class="shoot">{shoot.title}</a>
-    {/each}
-  </nav>
+	<!-- nav with shoots and projects -->
+	<nav class="projects-list">
+		{#each projects as project, i}
+			<a
+        href={project.link}
+        class="project"
+        class:inactive={i != 0} 
+        class:active={i == 0}
+			>
+        {project.title}
+      </a>
+		{/each}
+	</nav>
 
-  <!-- footer with links to work and info -->
-  <footer class="footer">
-    <!-- list of work done -->
-    <div class="work">
-      <a href="/jpeg-city" class="work-item">JPEG CITY</a>
-      <a href="/Talks" class="work-item">Talks</a>
-      <a href="/contact" class="work-item">About & Contact</a>
-    </div>
-    <!-- socail links -->
-    <div class="social">
-    </div>
-  </footer>
+	<!-- footer with links to work and info -->
+	<footer class="footer">
+		<!-- list of work done -->
+		<div class="work">
+			<a href="/jpeg-city" class="work-item">JPEG CITY</a>
+			<a href="/Talks" class="work-item">Talks</a>
+			<a href="/contact" class="work-item">About & Contact</a>
+		</div>
+		<!-- socail links -->
+		<div class="social" />
+	</footer>
 </div>
 
 <style lang="scss">
-.sidebar{
-  width: fit-content;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  padding: 24px 0;
-  position: sticky;
-  top: 0;
-  left: 0;
-  flex-shrink: 0;
-  flex-grow: 0;
-}  
-.logo{
-  text-transform: uppercase;
-  font-size: 36px;
-  letter-spacing: 4px;
-  line-height: 1;
-}
-.subtitle{
-  font-size: 16px;
-  text-transform: capitalize;
-  color: gray;
-  margin-top: 12px;
-}
+	.sidebar {
+		width: fit-content;
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		padding: 24px 0;
+		position: sticky;
+		top: 0;
+		left: 0;
+		flex-shrink: 0;
+		flex-grow: 0;
+	}
+	.logo {
+		text-transform: uppercase;
+		font-size: 36px;
+		letter-spacing: 4px;
+		line-height: 1;
+	}
+	.subtitle {
+		font-size: 16px;
+		text-transform: capitalize;
+		color: gray;
+		margin-top: 12px;
+	}
 
-.shoots-list{
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-}
-.shoot{
-  text-transform: capitalize;
-  font-size: 20px;
-  font-weight: 500;
-  cursor: pointer;
-  &:hover{
-    text-decoration: line-through;
-    scale: 1.05;
-  }
-}
+	.projects-list {
+		display: flex;
+		flex-direction: column;
+		gap: 16px;
+	}
+	.project {
+		text-transform: capitalize;
+		font-size: 20px;
+		font-weight: 500;
+		cursor: pointer;
+	}
+	.inactive {
+		&:hover {
+			text-decoration: line-through;
+			scale: 1.05;
+		}
+	}
 
-.work{
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-}
-.work-item{
-  font-size: 16px;
-  cursor: pointer;
-  &:hover{
-    color: gray;
-  }
-}
+	.active {
+		background: linear-gradient(
+			135deg,
+			#ff00d2,
+			#fed90f,
+			#00a2ff,
+			#09f1b8,
+			#ff00d2,
+			#fed90f,
+			#00a2ff,
+			#09f1b8
+		);
+		background-size: 300% 300%;
+		background-clip: text;
+		-webkit-text-fill-color: transparent;
+		animation: gradient 8s ease-in-out infinite;
+	}
+
+	.work {
+		display: flex;
+		flex-direction: column;
+		gap: 8px;
+	}
+	.work-item {
+		font-size: 16px;
+		cursor: pointer;
+		&:hover {
+			color: gray;
+		}
+	}
+
+	@keyframes gradient {
+		0% {
+			background-position: 0% 50%;
+		}
+		50% {
+			background-position: 100% 50%;
+		}
+		100% {
+			background-position: 0% 50%;
+		}
+	}
 </style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -6,80 +6,95 @@
 	const projects = [
 		{
 			name: 'Pepsi Black',
-			link: ''
+			link: '',
+			img: 'https://images.squarespace-cdn.com/content/v1/5b27a34be17ba335ea8d1983/1592154457702-JLIJK8STYUPGC164IW7Z/TAREK123-2.JPG?format=500w'
 		},
 		{
 			name: "Adidas 'Watch Us Move'",
-			link: ''
+			link: '',
+			img: 'https://images.squarespace-cdn.com/content/v1/5b27a34be17ba335ea8d1983/1632759176142-UHS9PAQM4YBGGY0NPDS0/TAREK749.jpg?format=500w'
 		},
 		{
 			name: `'Florida' // Marwan Moussa`,
-			link: ''
+			link: '',
+			img: 'https://images.squarespace-cdn.com/content/v1/5b27a34be17ba335ea8d1983/1616626787994-WBUGEOMB8WVY4H6VGUDG/TAREK190.jpg?format=500w'
 		},
 		{
 			name: `Ehab Amin for Red Bull`,
-			link: ''
+			link: '',
+			img: 'https://images.squarespace-cdn.com/content/v1/5b27a34be17ba335ea8d1983/1632757963070-9UBHPUCT1I0VCX4S8XDG/TAREK973.jpg?format=500w'
 		},
 		{
 			name: `'Free' // Marwan Pablo`,
-			link: ''
+			link: '',
+			img: 'https://images.squarespace-cdn.com/content/v1/5b27a34be17ba335ea8d1983/1592150835731-ODUGJGSNV89XJS8VOYBV/TAREK73.JPG?format=500w'
 		},
 		{
 			name: `'Dorak Gai' // Wegz`,
-			link: ''
+			link: '',
+			img: 'https://images.squarespace-cdn.com/content/v1/5b27a34be17ba335ea8d1983/1591992882747-633S2OUL8ZS6U9MLDXW4/TAREK024.JPG?format=500w'
 		},
 		{
 			name: 'Mousv In Ismalia',
-			link: ''
+			link: '',
+			img: 'https://images.squarespace-cdn.com/content/v1/5b27a34be17ba335ea8d1983/1632760038698-33P4Y0GWLU72G4X1OW07/TAREK966.jpg?format=500w'
 		},
 		{
 			name: 'PUBG x Wegz',
-			link: ''
+			link: '',
+			img: 'https://images.squarespace-cdn.com/content/v1/5b27a34be17ba335ea8d1983/1632760271831-S7REQXX9F9RVZYWA5HDH/TAREK451.jpg?format=500w'
 		},
 		{
 			name: 'One & Four Studio AW20',
-			link: ''
+			link: '',
+			img: ''
 		},
 		{
 			name: 'Rebel Cairo',
-			link: ''
+			link: '',
+			img: ''
 		},
 		{
 			name: 'Pier 88 x LUCID',
-			link: ''
+			link: '',
+			img: ''
 		},
 		{
 			name: 'Suzanne SS19',
-			link: ''
+			link: '',
+			img: ''
 		},
 		{
 			name: 'UNITY',
-			link: ''
+			link: '',
+			img: ''
 		}
 	];
 
 	let projectsList: HTMLUListElement;
+	let projectsImages: HTMLDivElement;
 	let projectsListHeight: number;
 
 	onMount(() => {
 		const $projects = projectsList.querySelectorAll('.project');
+		const $projectsLinks = document.querySelectorAll('.project-link');
+		const $projectsImages = document.querySelectorAll('.project-image');
 
-      let projectHeight : number
-      let projectsHeight : number
-      let lastProjectY : number
+		let projectHeight: number;
+		let projectsHeight: number;
+		let lastProjectY: number;
 
-    const calculateElements = () =>
-    {
-      projectHeight = $projects[0].clientHeight;
-      projectsHeight = projectHeight * $projects.length;
-      lastProjectY = projectsHeight - projectHeight;
-    }
-    calculateElements()
+		const calculateElements = () => {
+			projectHeight = $projects[0].clientHeight;
+			projectsHeight = projectHeight * $projects.length;
+			lastProjectY = projectsHeight - projectHeight;
+		};
+		calculateElements();
 
 		let scrollY: number = 0;
 		let y: number = 0;
-    let scrollSpeed : number = 0
-    let oldScroll : number = 0
+		let scrollSpeed: number = 0;
+		let oldScroll: number = 0;
 
 		// POSITION THE PROJECT Y POSITION // SCROLL PROJECTS
 		const positionProject = (scroll: number) => {
@@ -97,46 +112,103 @@
 			y = lerp(y, scrollY, 0.1);
 			positionProject(y);
 
-      oldScroll = y
-      scrollSpeed = Math.round(scrollY - y)
+			oldScroll = y;
+			scrollSpeed = Math.round(scrollY - y);
 
-      gsap.to($projects,
-      {
-        scale : () =>{
-          let speed = Math.abs(scrollSpeed * .0025)
-          let scale = gsap.utils.clamp (1, 2, speed)
-          return scale
-        },
-        duration : 1
-      })
+			gsap.to($projectsLinks, {
+				scale: () => {
+					let speed = Math.abs(scrollSpeed * 0.0025);
+					let scale = gsap.utils.clamp(1, 2, speed);
+					return scale;
+				},
+				duration: 0.7
+			});
 
 			requestAnimationFrame(updatePosition);
 		};
 		updatePosition();
 
 		// WHEEL EVENT LISTENER
-		projectsList.addEventListener('wheel', (e) =>
-    {
+		projectsList.addEventListener('wheel', (e) => {
 			scrollY -= e.deltaY;
 		});
 
-    // RESIZE EVENT LISTENER
-    window.addEventListener('resize', () =>
-    {
-      calculateElements()
-    })
+		// RESIZE EVENT LISTENER
+		window.addEventListener('resize', () => {
+			calculateElements();
+		});
 
+		// MOUSE IN EVENT LISTENER
 	});
+
+	const getImage = (e : any) =>
+	{
+		const key = parseInt(e.target.dataset.projectKey);
+		const img = projectsImages.children[key] as HTMLElement;
+		return img
+	}
+
+	const viewProjectImage = (e: any) => {
+		const img = getImage(e)
+
+		gsap.from(img, {
+			scale : .8,
+			rotate : "random(10,20)",
+		})
+		img.style.opacity = '1';
+	};
+
+
+	const hideProjectImage = (e: any) => 
+	{
+		const img = getImage(e)
+		img.style.opacity = '0';
+	};
+
+	const moveImage = (e : any) =>
+	{
+		let mousePosition =
+		{
+			// x : (e.x - ((window.innerWidth - e.target.clientWidth) / 2)) / e.target.clientWidth,
+			x : (e.x - e.target.offsetLeft) / e.target.clientWidth,
+			y : (e.y - ((e.target.scrollTop - e.target.clientHeight) / 2)) / e.target.clientHeight
+		}
+		const img = getImage(e);
+		gsap.set(img, {
+			x : mousePosition.x * 40,
+			duration : .3,
+			ease: 'slow'
+		})
+
+	}
+
+
 </script>
 
 <main class="projects">
 	<ul class="projects-list" bind:this={projectsList} bind:clientHeight={projectsListHeight}>
 		{#each projects as project, i}
-			<li class="project" style:transform={`translate(0, ${i*100}px)`}>
-				<a href={project.link} class="project-link">{project.name}</a>
+			<li class="project" style:transform={`translate(0, ${i * 100}px)`}>
+				<a
+					data-project-Name={project.name}
+					data-project-Key={i}
+					href={project.link}
+					class="project-link"
+					on:mouseenter={viewProjectImage}
+					on:mouseleave={hideProjectImage}
+					on:mousemove={moveImage}
+				>
+					{project.name}
+					</a>
 			</li>
 		{/each}
 	</ul>
+
+	<div class="projects-images" bind:this={projectsImages}>
+		{#each projects as project, i}
+			<img data-projectImageIndex={i} src={project.img} class="project-img" alt="" />
+		{/each}
+	</div>
 </main>
 
 <style lang="scss">
@@ -147,17 +219,54 @@
 		position: relative;
 	}
 	.project {
+		position: absolute;
 		width: 100%;
 		padding: 1vw 0;
-		position: absolute;
 		top: 0;
 		left: 0;
 		z-index: 1;
+		display: flex;
+		justify-content: center;
 	}
 	.project-link {
 		font-size: 6vw;
 		display: block;
 		text-align: center;
 		user-select: none;
+		position: relative;
+		mix-blend-mode: multiply;
+		&:before {
+			visibility: visible;
+			content: attr(data-project-name);
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 0;
+			overflow: hidden;
+			z-index: 2;
+			transition: width 0.7s cubic-bezier(1, 0, 0, 1);
+			white-space: nowrap;
+			color: red;
+		}
+		&:hover {
+			transform: scale(.8);
+			visibility: hidden;
+			transition: visibility 0s 0.7s linear;
+		}
+		&:hover::before {
+			width: 100%;
+		}
+	}
+
+	.project-img {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		width: 20vw;
+		aspect-ratio: 1/1.5;
+		z-index: -1;
+		object-fit: cover;
+		opacity: 0;
 	}
 </style>
